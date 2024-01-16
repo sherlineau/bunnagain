c5d69f3295c37f6609b16a115f7a9206c65c1465
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-16 17:50:29 -0600

    basic navigation

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,9 +1,44 @@
-import './index.css'
+import "./index.css";
+import { Link } from 'react-router-dom'
+import logo from '../../assets/placeholder.webp'
 
 const index = () => {
   return (
-    <div>navigation</div>
+      <div className="container">
+        <img src={logo} alt="" className="logo"/>
+        <nav className="navbar">
+          <div className="nav-item dropdown">
+            About
+            <div className="dropdown-content">
+              <Link to="/about-bunnagain" className="dropdown-item">Our Story</Link>
+              <Link to="/meet-the-team" className="dropdown-item">Meet the Team</Link>
+              <Link to="/faq" className="dropdown-item">FAQ</Link>
+            </div>
+          </div>
+          <div className="nav-item">
+            <Link to="/adopt">Adopt</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/admissions">Admissions</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/get-involved">Get Involved</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/donate">Donate</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/news">News</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/resources">Resources</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/contact">Contact Us</Link>
+          </div>
+        </nav>
+      </div>
   )
-}
+}; 
 
-export default index
+export default index;
