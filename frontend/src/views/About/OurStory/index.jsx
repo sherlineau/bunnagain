f8ca97bede8718262d317a24ce6d4c1b298be191
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-21 14:29:19 -0600

    about page content and container size style

--- frontend/src/views/About/OurStory/index.jsx
+++ frontend/src/views/About/OurStory/index.jsx
@@ -1,9 +1,47 @@
-import './index.css'
+import "./index.css";
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <section className="about">
+      <div className="container">
+        <h1>Our Story</h1>
+        <p>
+          Under the compassionate leadership of President Donna Rome, our
+          journey in rescuing rabbits began in 2012, culminating in the
+          establishment of BunnAgain Rabbit Rescue in 2015. Our mission revolves
+          around the rehabilitation and re-homing of neglected and abandoned
+          rabbits, with a concurrent commitment to fostering responsible
+          ownership for these often misunderstood animals.
+        </p>
+        <p>
+          At any given time, we provide care for approximately 20 rabbits,
+          relying solely on donations to support our cause. Ms. Rome&apos;s
+          dedication extends beyond her role, frequently covering veterinary
+          care and other essentials out of her own pocket. Committed to advocacy
+          and education, she actively engages with the community, participating
+          in events ranging from festivals to farmers markets.
+        </p>
+        <p>
+          As part of our commitment to responsible rabbit ownership, Ms. Rome
+          has instituted a unique &quot;Learn Before You Leap&quot; program. This
+          initiative allows prospective owners to explore rabbit ownership
+          before formal commitment and facilitates exchanges to ensure optimal
+          placements. Beyond rescue efforts, Ms. Rome, who also serves as a
+          staff member at a local elementary school, has introduced innovative
+          programs like the &quot;bunny box&quot; in the cafeteria, encouraging students
+          to donate vegetables that would otherwise go to waste. Additionally,
+          the school-wide incentive program, &quot;Breakfast with the Bunnies,&quot;
+          exemplifies our holistic approach to fostering compassion and
+          responsibility in the community. Join us in making a difference in the
+          lives of these adorable creatures and promoting a culture of informed
+          and caring rabbit ownership.
+        </p>
+        <p>
+          BunnAgain received recognition as a 501(c)(3) non-profit in July 2018.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default index
+export default index;
