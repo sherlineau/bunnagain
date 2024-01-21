e3edb7d6db95ae7b8b8a505b1d9904da2963bbd3
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-21 15:13:42 -0600

    Footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,9 +1,21 @@
-import './index.css'
+import "./index.css";
+import { FaFacebook } from "react-icons/fa";
 
 const index = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <button className="donate">Donate</button>
+      <div className="socials">
+        <a href="https://www.facebook.com/BunnAgain/" target="__blank" className="social-icons"><FaFacebook/></a>
+        {/* <a href="">svg</a>
+        <a href="">svg</a>
+        <a href="">svg</a>
+        <a href="">svg</a> */}
+      </div>
+      <p>Paradis, Lousiana | <a href="mailto:bunnagain@gmail.com">bunnagain@gmail.com</a></p>
+      
+    </div>
+  );
+};
 
-export default index
+export default index;
