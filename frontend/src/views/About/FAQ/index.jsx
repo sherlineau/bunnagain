8d61b76ec12ddc1140e174cb0786238c933e82b3
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-01-24 22:38:26 -0600

    import data from json and render accordian comp for each question

--- frontend/src/views/About/FAQ/index.jsx
+++ frontend/src/views/About/FAQ/index.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
+import Accordian from '../../../components/Accordian'
 import './index.css'
 
 const index = () => {
+  // initialize data state and fetch data from json
+  const [data, setData] = useState([])
+
+  // function to get data
+  const getData = async() => {
+    fetch("/faq.json")
+      .then(res => res.json())
+      .then(res => setData(res))
+  }
+
+  // on load, fetch data
+  useEffect( ()=> {
+    getData()
+  })
+
   return (
     <section>
       <h1 className="header">Frequently Asked Questions</h1>
-      <div className="container"></div>
+      <div className="container">
+        {
+          data.map((i,index) => {return <Accordian data={i} key={index}/>})
+        }
+      </div>
     </section>
   )
 }
