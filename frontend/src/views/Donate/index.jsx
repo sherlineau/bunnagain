9aa1a8eba4115dd8dd59ee08cddeeadaa82fe6c3
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-02-05 19:40:26 +0000

    donate page with placeholder content

--- frontend/src/views/Donate/index.jsx
+++ frontend/src/views/Donate/index.jsx
@@ -1,8 +1,30 @@
 import './index.css'
+import Header from "../../components/Header"
+import Button from "../../components/Button"
+import placeholder from '../../assets/images/placeholder-image.webp'
 
 const index = () => {
   return (
-    <section>donate</section>
+    <section className='donate'>
+      <Header title="Donate"/>
+      <div className="container">
+        <p className="grid-item">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci perspiciatis debitis ab repudiandae tempore, accusantium, fuga nostrum pariatur nihil, voluptate ipsum voluptates quas ad officiis dolores nisi deleniti minima quis!
+        </p>
+          <div className="options grid-item">
+            <img src={placeholder} alt="" />
+            <Button text="Amazon Smile"/>
+          </div>
+          <div className="options grid-item">
+            <img src={placeholder} alt="" />
+            <Button text="Venmo"/>
+          </div>
+          <div className="options grid-item">
+            <img src={placeholder} alt="" />
+            <Button text="Paypal"/>
+          </div>
+      </div>
+    </section>
   )
 }
 
