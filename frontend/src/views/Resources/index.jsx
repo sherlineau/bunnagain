56125afff9490be37ad236900fdf80875cca5e6d
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-02-06 19:44:45 -0600

    resources page in progress, need to add content

--- frontend/src/views/Resources/index.jsx
+++ frontend/src/views/Resources/index.jsx
@@ -1,8 +1,28 @@
 import './index.css'
+import Header from '../../components/Header'
+import Accordian2 from '../../components/Accordian2'
 
 const index = () => {
   return (
-    <section>resources</section>
+    <section className='resources'>
+      <Header title="Rabbit 101" />
+      <div className="container">
+
+      <Accordian2 title="8 common myths about rabbits">
+        <ol>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+          <li>asdf</li>
+        </ol>
+      </Accordian2>
+
+      </div>
+    </section>
   )
 }
 
