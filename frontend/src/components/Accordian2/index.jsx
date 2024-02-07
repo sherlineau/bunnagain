5a72dfaa7dd719ab6396178a70d69f8e3c8a02e8
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-02-06 19:43:46 -0600

    duplicate accordian for resources -> different data props

---
+++ frontend/src/components/Accordian2/index.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import './index.css'
+
+
+const Index = (props) => {
+  // destructure
+  const { title, children }  = props
+  const [expanded, setExpanded] = useState(false)
+
+
+  return (
+    <div className='accordian'>
+        <div onClick={(e)=> setExpanded(!expanded)} className="row space-between">
+            <h4>{title}</h4>
+            {expanded ? <FaMinus className="icon" /> : <FaPlus className="icon"/>}
+        </div> 
+            
+        <div className={`content ${expanded ? "open": "close"}`}>
+          {children}
+        </div>
+
+        <div className="line"></div>
+    </div>
+  )
+}
+
+export default Index
+
