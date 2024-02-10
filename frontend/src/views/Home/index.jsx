2043b908c9df160aada438278c1c0238e5368abf
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-02-10 09:43:38 -0600

    placeholder content

--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -1,8 +1,17 @@
 import './index.css'
+import placeholder from "../../assets/images/placeholder-image.webp"
 
 const index = () => {
   return (
-    <section>home page</section>
+    <section>
+      <div className="container">
+
+      <img src={placeholder} alt="" />
+      <p> 
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem eius perspiciatis blanditiis id animi maiores, delectus possimus sequi suscipit molestiae! Qui ex numquam facilis? Quis cumque eum expedita magnam voluptatem id excepturi quisquam alias neque unde quo, dolores dolor sequi minima recusandae corporis vero provident pariatur, dolore doloribus. Esse, temporibus perspiciatis? Maiores magni tempore neque! Eius, vero a, aliquam molestias vitae assumenda cumque harum commodi veritatis illum sint. Excepturi, asperiores, ullam omnis molestias labore minima quos cum, eos beatae distinctio voluptatum. Nostrum reprehenderit harum iure sint voluptatum atque minus maiores voluptate. Aliquam nulla accusamus sequi enim in soluta nostrum, doloremque ducimus temporibus, quidem facere nemo modi qui saepe quae harum minus sit impedit nobis veniam tenetur ratione! Architecto sed corrupti doloribus vel, libero neque, nisi optio incidunt laboriosam eveniet vero iste nulla voluptate veritatis deleniti aliquid cumque mollitia? Odit a rerum deleniti voluptate dolore, eaque adipisci eius fuga ex molestias!
+      </p>
+      </div>
+    </section>
   )
 }
 
