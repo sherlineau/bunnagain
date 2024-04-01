f5e2c01c962add3639d4de7264e621b37f06b974
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2024-04-01 15:12:52 -0500

    updated faq questions

--- frontend/src/views/About/FAQ/index.jsx
+++ frontend/src/views/About/FAQ/index.jsx
@@ -1,35 +1,51 @@
-import { useEffect, useState } from 'react'
-import Accordian from '../../../components/Accordian'
-import Header from '../../../components/Header'
+import { useEffect, useState } from "react";
+import Accordian from "../../../components/Accordian";
+import Header from "../../../components/Header";
 
-import './index.css'
+import "./index.css";
 
 const index = () => {
   // initialize data state and fetch data from json
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   // function to get data
-  const getData = async() => {
+  const getData = async () => {
     fetch("/faq.json")
-      .then(res => res.json())
-      .then(res => setData(res))
-  }
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  };
 
   // on load, fetch data
-  useEffect( ()=> {
-    getData()
-  })
+  useEffect(() => {
+    getData();
+  });
 
   return (
-    <section className='faq'>
-      <Header title="Frequently Asked Questions"/>
+    <section className="faq">
+      <Header title="Frequently Asked Questions" />
       <div className="container">
-        {
-          data.map((i,index) => {return <Accordian data={i} key={index}/>})
-        }
+        <div className="text-container">
+          <p>
+            Here, we&apos;ve compiled answers to some of the most common
+            questions about our organization, our mission, and how you can get
+            involved. Whether you&apos;re looking to adopt a rabbit, volunteer
+            with us, or simply learn more about responsible rabbit ownership, we
+            hope you&apos;ll find the information you need here. If you have a
+            question that&apos;s not answered below, please don&apos;t hesitate
+            to reach out to us. Your support means the world to us and the
+            rabbits in our care.
+          </p>
+          <p>
+            Thank you for your interest in BunnAgain Rabbit Rescue and
+            Education!
+          </p>
+        </div>
+        {data.map((i, index) => {
+          return <Accordian data={i} key={index} />;
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default index
+export default index;
